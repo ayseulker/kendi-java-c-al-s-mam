8bb1555be6c247591c72a13e767da98367b75849
ayseulker <aysenrulkerr@gmail.com>
2023-02-13 22:54:13 +0300

    .

--- 50. 2'lik Sayıdan 10'luğa Çevirme Uygulaması/app.js
+++ 50. 2'lik Sayıdan 10'luğa Çevirme Uygulaması/app.js
@@ -9,7 +9,9 @@ function converBinaryToDecinal(binary){
     let toplam =0;
     let ust =0;
     for(let i =binary.length-1; i>=0 ; i--){
-        toplam +=Number(binary.charAt(i)) * Math.pow(2,ust);
+        if(Number(binary.charAt(i))!=0){
+            toplam +=Number(binary.charAt(i)) * Math.pow(2,ust);
+        }
         ust++;
     }
 
